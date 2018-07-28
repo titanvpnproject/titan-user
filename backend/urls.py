27e2fc69f443df3a6f288918ac444c066ed502b3
ahn <93immm@naver.com>
2018-07-28 17:55:00 +0900

    remove drf, opt

--- backend/urls.py
+++ backend/urls.py
@@ -1,33 +1,16 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .djangoapps.drf.views import SampleView
-from .djangoapps.drf.views import SimpleUserView
-from .djangoapps.drf.views import SimpleUserAddView
-
-from .djangoapps.login import views as loginViews
 from .djangoapps.sample import views as SampleViews
-from .djangoapps.logout import views as logoutViews
-
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='happy API')
+from .djangoapps.login import views as LoginViews
+from .djangoapps.logout import views as LogoutViews
 
 urlpatterns = [
-    # rest-api
-
-    url('api_sample$', SampleView.as_view()),
-    url('api_simpleUser$', SimpleUserView.as_view()),
-    url('api_simpleAddUser$', SimpleUserAddView.as_view()),
-
-    # rest-swagger
-    url('swagger$', schema_view),
-
     # main-url
     url('sample$', SampleViews.sample, name='sample'),
     url('vuejs$', SampleViews.vuejs, name='vuejs'),
     url('vueservice$', SampleViews.vueService, name='vueService'),
 
-    url('login$', loginViews.login, name='login'),
-    url('logout$', logoutViews.logout, name='logout'),
+    url('login$', LoginViews.login, name='login'),
+    url('logout$', LogoutViews.logout, name='logout'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    #path('admin/', admin.site.urls),
     url(r'^', include('backend.urls')),
 ]
