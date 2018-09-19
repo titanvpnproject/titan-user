df7766b2c5143054262f1633c397c4589514a2c1
ahn <93immm@naver.com>
2018-09-19 13:22:44 +0900

    fix: remove drf

--- main/settings.py
+++ main/settings.py
@@ -15,8 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_swagger',
     'backend',
 ]
 
